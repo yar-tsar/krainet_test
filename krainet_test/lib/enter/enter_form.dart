b84caa9c2b1f9c1ce9417da859864327a7b34c94
Yaraslau <yaraslau.tsar@gmail.com>
2022-11-01 21:17:02 +0300

    the validation of filled form with new function named filled

--- krainet_test/lib/enter/enter_form.dart
+++ krainet_test/lib/enter/enter_form.dart
@@ -23,15 +23,13 @@ class _EnterFormState extends State<EnterForm> {
     super.dispose();
   }
 
-  void filled() {
-    if (_formKey.currentState!.validate() == true) {
-      setState(() {
-        _isFormFilled = true;
+  void filled() => setState(() {
+        if (_formKey.currentState!.validate() == true) {
+          _isFormFilled = true;
+        } else {
+          _isFormFilled = false;
+        }
       });
-    } else {
-      _isFormFilled = false;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,67 +42,53 @@ class _EnterFormState extends State<EnterForm> {
             height: 15,
           ),
           TextFormField(
-            controller: _emailController,
-            decoration: const InputDecoration(
-              labelText: 'e-mail',
-              helperText: '',
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your e-mail';
-              }
-              String p =
-                  "[a-zA-Z0-9+._%-+]{1,256}@[a-zA-Z0-9][a-zA-Z0-9-]{0,64}(.[a-zA-Z0-9][a-zA-Z0-9-]{0,25})+";
-              RegExp regExp = RegExp(p);
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'e-mail',
+                helperText: '',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your e-mail';
+                }
+                String p =
+                    "[a-zA-Z0-9+._%-+]{1,256}@[a-zA-Z0-9][a-zA-Z0-9-]{0,64}(.[a-zA-Z0-9][a-zA-Z0-9-]{0,25})+";
+                RegExp regExp = RegExp(p);
 
-              if (regExp.hasMatch(value)) return null;
+                if (regExp.hasMatch(value)) return null;
 
-              return 'Please enter correct e-mail';
-            },
-            onChanged: (value) => setState(() {
-              _formKey.currentState!.validate();
-              if (_formKey.currentState!.validate() == true) {
-                _isFormFilled = true;
-              } else {
-                _isFormFilled = false;
-              }
-            }),
-          ),
+                return 'Please enter correct e-mail';
+              },
+              onChanged: (value) => filled()),
           TextFormField(
-            controller: _passController,
-            obscureText: _isPasswordObscured,
-            decoration: InputDecoration(
-              labelText: 'password',
-              helperText: '',
-              suffixIcon: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _isPasswordObscured = !_isPasswordObscured;
-                  });
-                },
-                icon: Icon(
-                  _isPasswordObscured ? Icons.visibility : Icons.visibility_off,
-                  color: Colors.black,
+              controller: _passController,
+              obscureText: _isPasswordObscured,
+              decoration: InputDecoration(
+                labelText: 'password',
+                helperText: '',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordObscured = !_isPasswordObscured;
+                    });
+                  },
+                  icon: Icon(
+                    _isPasswordObscured
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    color: Colors.black,
+                  ),
                 ),
               ),
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your password';
-              }
-              if (value.length > 5) return null;
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                if (value.length > 5) return null;
 
-              return 'Password must include 6 symbols';
-            },
-            onChanged: (value) => setState(() {
-              _formKey.currentState!.validate();
-              if (_formKey.currentState!.validate() == true) {
-                _isFormFilled = true;
-              } else {
-                _isFormFilled = false;
-              }
-            }),
-          ),
+                return 'Password must include 6 symbols';
+              },
+              onChanged: (value) => filled()),
           const SizedBox(
             height: 10,
           ),
